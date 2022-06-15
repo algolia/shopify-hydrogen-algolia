59eddbc816a93e51e1563efa641427a03d7e2edc
François Chalifour <francoischalifour@users.noreply.github.com>
2022-06-15 12:27:30 +0200

    fix(search): batch initial Insights call with Search call

--- src/components/search/AlgoliaInsights.client.jsx
+++ src/components/search/AlgoliaInsights.client.jsx
@@ -3,50 +3,14 @@
  * https://www.algolia.com/doc/api-reference/widgets/insights/react-hooks/?client=js
  */
 import {createInsightsMiddleware} from 'instantsearch.js/es/middlewares';
-import {useEffect} from 'react';
-import {useConnector} from 'react-instantsearch-hooks-web';
+import {useLayoutEffect} from 'react';
+import {useInstantSearch} from 'react-instantsearch-hooks-web';
 import algoConfig from '../../../algolia.config.json';
 
-const connectMiddleware = (renderFn, unmountFn) => (widgetParams) => ({
-  init(initOptions) {
-    renderFn(
-      {
-        ...this.getWidgetRenderState(initOptions),
-        instantSearchInstance: initOptions.instantSearchInstance,
-      },
-      true,
-    );
-  },
-
-  render(renderOptions) {
-    const renderState = this.getWidgetRenderState(renderOptions);
-
-    renderFn(
-      {
-        ...renderState,
-        instantSearchInstance: renderOptions.instantSearchInstance,
-      },
-      false,
-    );
-  },
-
-  getWidgetRenderState(renderOptions) {
-    return {
-      use: (...args) => renderOptions.instantSearchInstance.use(...args),
-      unuse: (...args) => renderOptions.instantSearchInstance.unuse(...args),
-      widgetParams,
-    };
-  },
-
-  dispose() {
-    unmountFn();
-  },
-});
-
 export function Insights() {
-  const {use} = useConnector(connectMiddleware);
+  const {use} = useInstantSearch();
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     const insightsClient = window.aa;
     insightsClient('init', {
       appId: algoConfig.appId,
@@ -57,7 +21,7 @@ export function Insights() {
       insightsClient,
     });
 
-    use(middleware);
+    return use(middleware);
   }, [use]);
 
   return null;
